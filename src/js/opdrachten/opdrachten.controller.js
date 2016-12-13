464fa0a21ef33952a108f01ec43e40ad92a8f0db
simondegheselle <simondegheselle@hotmail.com>
2016-12-13 21:15:58 +0100

    opdrachten studenten overzicht + tabs

--- src/js/opdrachten/opdrachten.controller.js
+++ src/js/opdrachten/opdrachten.controller.js
@@ -1,18 +1,39 @@
 class OpdrachtenCtrl {
-  constructor(User, Opdrachten, AppConstants, $scope) {
-    'ngInject';
-    Opdrachten
-      .getAll()
-      .then(
-        (opdrachten) => {
-          this.opdrachtenLoaded = true;
-          this.opdrachten = opdrachten;
-          console.log(opdrachten);
+    constructor(User, Opdrachten, AppConstants, $scope) {
+        'ngInject';
+        console.log(User.current);
+        this.opdrachtenType = 'to-do';
+        this._Opdrachten = Opdrachten;
+        this._User = User;
+        this.opdrachtType = 'to-do';
+        this.setList(this.opdrachtType);
+    }
+
+    setList(type) {
+        this.opdrachtType = type;
+        if (type === 'to-do') {
+            this._Opdrachten
+                .getAll(this._User.current)
+                .then(
+                    (opdrachten) => {
+                        this.opdrachten = opdrachten.filter(function(val) {
+                            return val.afgewerkt === false || val.afgewerkt === null;
+                        });
+                    }
+                );
         }
-      );
-
-
-  }
+        if (type === 'done') {
+            this._Opdrachten
+                .getAll(this._User.current)
+                .then(
+                    (opdrachten) => {
+                        this.opdrachten = opdrachten.filter(function(val) {
+                            return val.afgewerkt === true;
+                        });
+                    }
+                );
+        }
+    }
 }
 
 export default OpdrachtenCtrl;
